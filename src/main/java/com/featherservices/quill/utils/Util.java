fa0c0dbff47ab7d5d60e69f8961400e965dbe1af
Levitate <levitatewebhost@hotmail.com>
2024-06-30 22:39:37 -0300

    added: string equation utility.

--- src/main/java/com/featherservices/quill/utils/Util.java
+++ src/main/java/com/featherservices/quill/utils/Util.java
@@ -4,6 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.Random;
 import java.util.UUID;
 
@@ -19,6 +22,23 @@ public class Util {
                 "{SkullOwner:{Id:[I;" + (less * most) + "," + (less >> 23) + "," + (most / less) + "," + (most * 8731) + "],Properties:{textures:[{Value:\"" + value + "\"}]}}}"
         );
     }
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    public static double evalEquation(String input) {
+        final ScriptEngineManager mgr = new ScriptEngineManager();
+        final ScriptEngine engine = mgr.getEngineByName("JavaScript");
+
+        try {
+            final String stringResult = engine.eval(input).toString();
+            return Double.parseDouble(stringResult);
+        }
+        catch (ScriptException ex) {
+            ex.printStackTrace();
+        }
+
+        return 0;
+    }
+
 
     public static int getRandomNumber(int min, int max) {
         return new Random().nextInt((max - min) + 1) + min;
